da3b0689dd2529a7f0a3ea936c14ec15b5a9e0a9
Ivan Ramirez <ramirezfranciscoivan@gmail.com>
2018-08-22 21:14:56 -0600

    Initial Commit

---
+++ Jorney Afternoon 22 Aug 2018/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Jorney Afternoon 22 Aug 2018
+//
+//  Created by Ivan Ramirez on 8/22/18.
+//  Copyright © 2018 ramcomw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
